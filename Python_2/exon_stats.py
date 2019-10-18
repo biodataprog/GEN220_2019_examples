49399aaf99a9a1a020e51e136a156728f6f171b8
Jason Stajich <jasonstajich.phd@gmail.com>
2019-10-18 16:16:45 -0700

    start of code

--- Python_2/exon_stats.py
+++ Python_2/exon_stats.py
@@ -1,3 +1,13 @@
 #!/usr/bin/env python
 
 filename="/bigdata/gen220/shared/simple/rice_random_exons.bed"
+with open(filename,"r") as fh:
+	for line in fh:
+#		print(line)
+		list = line.strip().split("\t")
+#		print(list)
+		start = int(list[1])
+		end   = int(list[2])
+		exonlen = end - start + 1
+		print(exonlen)
+#		break
