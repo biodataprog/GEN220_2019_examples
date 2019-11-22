77cd69b7cb5581bb2bb5cbe5ffea3e644be961f2
Jason Stajich <jasonstajich.phd@gmail.com>
2019-11-22 15:03:03 -0800

    support orthogroup summary

--- Bioinformatics_2/parse_orthofinder.py
+++ Bioinformatics_2/parse_orthofinder.py
@@ -1,24 +1,28 @@
 #!/usr/bin/env python3
+import csv
 
 orthogroups = "Orthogroups.csv"
 
 groups = {}
-with open(orthogroups,"r") as ortho:
-    firstline = next(ortho)
-    header = firstline.strip().split("\t")
-    print(header)
-    for line in ortho:
-        row = line.strip().split("\t")
+with open(orthogroups,"r") as ortho, open("Orthogroups_counts.tab","w") as ogout:
+    rdr = csv.reader(ortho,delimiter="\t")
+    wrt = csv.writer(ogout,delimiter="\t")
+    header = next(rdr)
+    header[0] = "FAMILY"
+    wrt.writerow(header)
+    for row in rdr:
         name = row[0]
+        # initialize a row 0s -with as many cols as we need
+        # its -1 because the first col in the row is the name
+        # of the ortholog group
+        groups[name] = [0] * (len(row)-1)
         for i in range(1,len(row)):
             col = row[i]
             genes = col.split(", ")
-#            print(genes)
-            if name in groups:
-                groups[name][i] = str(len(genes))
-            else:
-                groups[name] = [0,0,0,0]
-                groups[name][i] = str(len(genes))
+            groups[name][i-1] = len(genes)
 
-for group in groups:
-    print(group, "\t".join(groups[group][1:4]))
+    wrt = csv.writer(ogout,delimiter="\t")
+    for group in groups:
+        row = [group]
+        row.extend(groups[group])
+        wrt.writerow(row)
