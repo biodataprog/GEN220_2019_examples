22f18e3ec9ac1bbbe7c935d38bf6b1d620ec29f3
Jason Stajich <jasonstajich.phd@gmail.com>
2019-11-22 14:55:14 -0800

    worm sequences

---
+++ Bioinformatics_5/download_worm_seqs.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/bash
+curl -L ftp://ftp.wormbase.org/pub/wormbase/species/c_elegans/sequence/transcripts/c_elegans.PRJNA13758.WS260.transposon_transcripts.fa.gz > transposon.fa.gz
+curl -L ftp://ftp.wormbase.org/pub/wormbase/species/c_elegans/sequence/transcripts/c_elegans.PRJNA13758.WS260.mRNA_transcripts.fa.gz > mRNA.fa.gz 
